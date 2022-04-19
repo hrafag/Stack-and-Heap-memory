484781b821a1ebf58a025e2064b68ad6502e5fb0
hrafag <afagnd@code.edu.az>
2022-04-19 21:09:58 +0400

    added

---
+++ ConsoleApp6/ConsoleApp6/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 34, 5, 32, 7, 205, 4 };
+            int result = 1;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] >= result)
+                {
+                    result = arr[i];
+                }
+                        }
+            Console.WriteLine("the largest numbet is " + result);
+        }
+    }
+}
